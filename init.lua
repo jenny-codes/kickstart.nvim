476a2fae99d28a0033ae10019bbed4bb29c8ac21
Jenny Shih <jinghua.shih@gmail.com>
2024-08-21 13:53:31 -0400

    Add sorbet as a lsp

--- init.lua
+++ init.lua
@@ -572,6 +572,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         ruby_lsp = {},
+        sorbet = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
